ee77cebef443b19a11efd811be3c3d023a2566f8
nj005py <py5555@hotmail.com>
2021-12-12 20:05:41 +0800

    update 330

--- app/src/main/java/org/phantancy/fgocalc/common/Constant.java
+++ app/src/main/java/org/phantancy/fgocalc/common/Constant.java
@@ -13,13 +13,13 @@ import java.util.Map;
 
 public class Constant {
     //数据库版本
-    public static final int DATABASE_VERSION= 76;
+    public static final int DATABASE_VERSION= 77;
 
     //handler
     public static final int CHECK_APP_VERSION = 666;
 
     //头像最新ID
-    public static final int AVATAR_LATEST_ID = 329;
+    public static final int AVATAR_LATEST_ID = 330;
 
     //默认头像下载地址
     public static final String AVATAR_BASE_URL = "https://gitee.com/nj005py/fgocalc/raw/master/svt/";
